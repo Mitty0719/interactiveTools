804b3072911d839108fc36a012fb4aec256016e2
jinuLee <62509499+Mitty0719@users.noreply.github.com>
2023-05-25 14:44:51 +0900

    Update horizontalSlider.js

--- horizontalSlider.js
+++ horizontalSlider.js
@@ -1,14 +1,17 @@
 class HorizontalSlider {
-  static init ({id, itemWidth = '20vw', leftButton = true, rightButton = true}) {
-
+  static init ({id, interval, timeout = 1000, leftButton = true, rightButton = true}) {
     const container = document.querySelector(`#${id}`);
     const items = [...container.children];
-    let index = 0;
+    const firstItem = items[0].cloneNode(true);
+    const lastItem = items[items.length - 1].cloneNode(true);
+    let index = -1;
+    let isMoving = false;
 
     container.innerHTML = '';
 
     const sliderBox = document.createElement('div');
     const sliderList = document.createElement('ul');
+    const sliderItems = [];
 
     sliderBox.className = 'horizontal-slider-box';
     sliderBox.style.position = 'relative';
@@ -19,21 +22,26 @@ class HorizontalSlider {
     sliderList.style.height = '100%';
     sliderList.style.display = 'flex';
     sliderList.style.position = 'absolute';
+    sliderList.style.transitionDuration = '1s'
 
     sliderBox.appendChild(sliderList);
     container.appendChild(sliderBox);
 
     const boxRect = sliderBox.getBoundingClientRect();
-    sliderBox.style.width = boxRect.width + 'px';
+    let itemWidth = boxRect.width;
+    // sliderBox.style.width = itemWidth + 'px';
 
-    for(const item of items) {
+    const copyItems = [lastItem, ...items, firstItem];
+    for(const item of copyItems) {
       const sliderItem = document.createElement('li');
       sliderItem.className = 'horizontal-slider-item';
-      sliderItem.style.width = boxRect.width + 'px';
+      sliderItem.style.width = itemWidth + 'px';
       sliderItem.style.height = '100%';
       sliderItem.appendChild(item);
       sliderList.appendChild(sliderItem);
+      sliderItems.push(sliderItem);
     }
+    sliderList.style.left = itemWidth * index + 'px';
 
     // create buttons
     if(leftButton) {
@@ -51,12 +59,12 @@ class HorizontalSlider {
       leftButtonCon.appendChild(leftButton);
       sliderBox.appendChild(leftButtonCon);
       
-      leftButton.addEventListener('click', () => {
-        index--;
-      })
+      leftButton.addEventListener('click', moveLeft)
     }
 
     if(rightButton) {
+      if(isMoving) return;
+
       const rightButtonCon = document.createElement('div');
       const rightButton = document.createElement('span');
 
@@ -71,9 +79,73 @@ class HorizontalSlider {
       rightButtonCon.appendChild(rightButton);
       sliderBox.appendChild(rightButtonCon);
 
-      rightButton.addEventListener('click', () => {
-        index++;
-      })
+      rightButton.addEventListener('click', moveRight)
+    }
+
+    if(interval) {
+      setInterval(() => {
+        moveLeft();
+      }, interval)
+    }
+    
+    window.addEventListener('resize', resize)
+
+    function moveLeft() {
+      if(isMoving) return;
+      isMoving = true;
+      setTimeout(() => {
+        isMoving = false;
+      }, timeout);
+
+      index--;
+      if(Math.abs(index) > items.length) {
+        index = 0;
+        sliderList.style.left = itemWidth * index + 'px';
+      } else {
+        sliderList.style.transitionDuration = `${timeout / 1000}s`;
+        sliderList.style.left = itemWidth * index + 'px';
+        if(Math.abs(index) == items.length) {
+          setTimeout(() => {
+            index = 0;
+            sliderList.style.transitionDuration = '0s';
+            sliderList.style.left = itemWidth * index + 'px';
+            isMoving = false;
+          }, timeout)
+        }
+      }
+    }
+
+    function moveRight() {
+      if(isMoving) return;
+      isMoving = true;
+      setTimeout(() => {
+        isMoving = false;
+      }, timeout);
+
+      index++;
+      if(index > 0) {
+        index = -(items.length);
+        sliderList.style.left = itemWidth * index + 'px';
+      } else {
+        sliderList.style.transitionDuration = `${timeout / 1000}s`;
+        sliderList.style.left = itemWidth * index + 'px';
+        if(index == 0) {
+          setTimeout(() => {
+            index = -(items.length);
+            sliderList.style.transitionDuration = '0s';
+            sliderList.style.left = itemWidth * index + 'px';
+            isMoving = false;
+          }, timeout)
+        }
+      }
+    }
+
+    function resize() {
+      const rect = sliderBox.getBoundingClientRect();
+      itemWidth = rect.width;
+      for(const item of sliderItems) {
+        item.style.width = itemWidth + 'px';
+      } 
     }
   }
 }
